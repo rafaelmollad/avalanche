40367e267172975e40f4aa6b1e98d0c7833aae54
rafaelmollad <rafaelmolladouglas@gmail.com>
2022-10-13 16:00:45 -0300

    Agregar notificación al cart widget

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,7 +1,25 @@
 import { ShoppingBagIcon } from '@heroicons/react/24/outline';
 
-const CartWidget = () => {
-  return <ShoppingBagIcon className='navbar-icon' />;
+const CartWidget = ({ cartCount }) => {
+  // Cambiar el fontSize de la notificación de acuerdo al número de dígitos del número
+  const cartCountLength = cartCount.toString().length;
+  const widgetNotificationStyles =
+    cartCountLength === 1 ? { fontSize: 12 } : { fontSize: 10 };
+
+  return (
+    <span className='cart-widget'>
+      {/* Sólo mostrar la notificación si hay algún producto en el carrito */}
+      {cartCount !== 0 && (
+        <span
+          className='cart-widget__notification'
+          style={widgetNotificationStyles}
+        >
+          {cartCount}
+        </span>
+      )}
+      <ShoppingBagIcon className='navbar-icon' />
+    </span>
+  );
 };
 
 export default CartWidget;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 import NavBar from './NavBar';
 
-function Header({ isMenuOpen, handleIsMenuOpen, handleIsSearchOpen }) {
+function Header({
+  isMenuOpen,
+  handleIsMenuOpen,
+  handleIsSearchOpen,
+  cartCount,
+}) {
   return (
     <header className='header'>
       <NavBar
         isMenuOpen={isMenuOpen}
         handleIsMenuOpen={handleIsMenuOpen}
         handleIsSearchOpen={handleIsSearchOpen}
+        cartCount={cartCount}
       />
     </header>
   );
